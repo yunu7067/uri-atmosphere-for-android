827245b0e264cafd6681691e7361c1ec9fe5322a
yunu7067 <yunu7067@gmail.com>
2021-05-09 19:34:05 +0900

    하단 바 추가

---
+++ app/src/main/java/com/example/uriatmosphere/Components/BottomNavigation.kt
@@ -0,0 +1,13 @@
+package com.example.uriatmosphere.Components
+
+import com.example.uriatmosphere.R
+import com.example.uriatmosphere.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class BottomNavigation {
+    fun BottomNavigation() {
+
+
+    }
+
+}
--- app/src/main/java/com/example/uriatmosphere/MainActivity.kt
+++ app/src/main/java/com/example/uriatmosphere/MainActivity.kt
@@ -2,12 +2,35 @@ package com.example.uriatmosphere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.uriatmosphere.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var test = "123";
-
+        onNavigationItemSelected()
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun onNavigationItemSelected(){
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val bottom_navigation = binding.bottomNavigation
+
+        bottom_navigation.setOnNavigationItemReselectedListener { item ->
+            when(item.itemId) {
+                R.id.bottom_navigation_menu_home -> {
+
+                }
+                R.id.bottom_navigation_menu_search -> {
+
+                }
+                R.id.bottom_navigation_menu_setting -> {
+
+                }
+            }
+        }
     }
 }
