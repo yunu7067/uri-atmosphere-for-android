02d8059e5308dc33b4ce3c726a74121a4cfd92b7
yunu7067 <yunu7067@gmail.com>
2021-05-09 22:40:09 +0900

    하단 바 디자인 및 페이지 변경 기능 구현중

---
+++ app/src/main/java/com/example/uriatmosphere/pages/search/SearchFragment.kt
@@ -0,0 +1,32 @@
+package com.example.uriatmosphere.pages.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.uriatmosphere.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
