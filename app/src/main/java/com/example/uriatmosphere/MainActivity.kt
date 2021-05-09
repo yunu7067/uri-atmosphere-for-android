8d7f231fe59f9be2633b14ea2af916c45a0f3b45
yunu7067 <yunu7067@gmail.com>
2021-05-09 18:00:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/uriatmosphere/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.uriatmosphere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var test = "123";
+
+        setContentView(R.layout.activity_main)
+    }
+}
