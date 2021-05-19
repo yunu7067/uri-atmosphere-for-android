939b4c3354f682d9973cc06547e513b04987a0d7
yunu7067 <yunu7067@gmail.com>
2021-05-19 21:32:31 +0900

    메인 페이지 디자인 카드 추가

--- app/src/main/java/com/example/uriatmosphere/MainActivity.kt
+++ app/src/main/java/com/example/uriatmosphere/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.uriatmosphere
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.uriatmosphere.component.stationview.StationViewAdapter
 import com.example.uriatmosphere.databinding.ActivityMainBinding
 import com.example.uriatmosphere.openapi.ArpltnInforInqireSvc
 import com.example.uriatmosphere.openapi.model.MsrstnAcctoRltmMesureDnstyData
@@ -13,24 +15,36 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var data: MsrstnAcctoRltmMesureDnstyData? = null
 
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var recyclerView: RecyclerView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        binding.testButton.setOnClickListener {
-            showData(it)
+        // Station Card 레이아웃 구성
+        viewManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        viewAdapter = StationViewAdapter()
+        recyclerView = binding.stationsView.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
         }
+
     }
 
-    private fun showData(view: View) {
+    private fun updateData() {
         thread {
             val arpltnInforInqireSvc = ArpltnInforInqireSvc()
             data = arpltnInforInqireSvc.MsrstnAcctoRltmMesureDnsty().get()
             Log.e("MainActivity", "clicked!")
             runOnUiThread {
-                binding.testTextview.text = data?.getItems()?.toString() ?: "null value!"
+                // 여기서 UI 상태 변경
+                data?.getItems()?.toString() ?: "null value!"
             }
         }
     }
---
+++ app/src/main/java/com/example/uriatmosphere/component/stationview/StationViewAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.uriatmosphere.component.stationview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.uriatmosphere.R
+
+class StationViewAdapter() : RecyclerView.Adapter<StationViewAdapter.StationViewHolder>() {
+    val totalCount = 5;
+
+    class StationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cardTitle: TextView = itemView.findViewById(R.id.title)
+        val cardIndex: TextView = itemView.findViewById(R.id.index)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StationViewHolder {
+        val cardView =
+            LayoutInflater.from(parent.context).inflate(R.layout.card_station, parent, false)
+        return StationViewHolder(cardView)
+    }
+
+    override fun onBindViewHolder(holder: StationViewHolder, position: Int) {
+        holder.cardTitle.text = "타이틀"
+        holder.cardIndex.text = "${position}/${totalCount}"
+    }
+
+    override fun getItemCount(): Int {
+        return totalCount
+    }
+}
