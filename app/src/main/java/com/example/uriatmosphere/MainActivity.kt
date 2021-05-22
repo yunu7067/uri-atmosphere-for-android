849fb85db33d5ae36fc8cd9334e83cabeaf57b8d
yunu7067 <yunu7067@gmail.com>
2021-05-22 23:59:47 +0900

    drawer navigation 추가

--- app/src/main/java/com/example/uriatmosphere/MainActivity.kt
+++ app/src/main/java/com/example/uriatmosphere/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.uriatmosphere
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.uriatmosphere.component.stationview.StationViewAdapter
@@ -19,12 +22,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var recyclerView: RecyclerView
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // Toolbar 레이아웃 구성
+        val appbar: Toolbar = binding.toolbar
+        setSupportActionBar(appbar)
 
         // Station Card 레이아웃 구성
         viewManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
@@ -34,7 +40,16 @@ class MainActivity : AppCompatActivity() {
             layoutManager = viewManager
             adapter = viewAdapter
         }
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            // 툴바의 네비게이션 버튼이 눌렸을 때
+            android.R.id.home->{
+                binding.drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun updateData() {
