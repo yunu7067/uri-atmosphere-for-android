cd9e18a23645bb6bd08bfc4e940c557aa09c53dd
yunu7067 <yunu7067@gmail.com>
2021-05-23 00:27:28 +0900

    디자인 일부 수정

--- app/src/main/java/com/example/uriatmosphere/component/stationview/StationViewAdapter.kt
+++ app/src/main/java/com/example/uriatmosphere/component/stationview/StationViewAdapter.kt
@@ -3,15 +3,17 @@ package com.example.uriatmosphere.component.stationview
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.uriatmosphere.R
 
 class StationViewAdapter() : RecyclerView.Adapter<StationViewAdapter.StationViewHolder>() {
-    val totalCount = 5;
+    val totalCount = 10;
 
     class StationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val cardTitle: TextView = itemView.findViewById(R.id.title)
+        val cardTitle: TextView = itemView.findViewById(R.id.card_header_title)
+        val cardClassification: ImageView = itemView.findViewById(R.id.card_header_icon)
         val cardIndex: TextView = itemView.findViewById(R.id.index)
     }
 
@@ -24,6 +26,8 @@ class StationViewAdapter() : RecyclerView.Adapter<StationViewAdapter.StationView
     override fun onBindViewHolder(holder: StationViewHolder, position: Int) {
         holder.cardTitle.text = "타이틀"
         holder.cardIndex.text = "${position + 1}/${totalCount}"
+        if(position != 0)
+            holder.cardClassification.setImageResource(R.drawable.ic_fluent_star_20_filled)
     }
 
     override fun getItemCount(): Int {
