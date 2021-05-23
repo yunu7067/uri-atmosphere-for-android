7fa499fff6cfde94708b4fb165ffb7ff43bae276
yunu7067 <yunu7067@gmail.com>
2021-05-23 21:34:37 +0900

    Navigation Drawer 동작 추가

--- app/src/main/java/com/example/uriatmosphere/MainActivity.kt
+++ app/src/main/java/com/example/uriatmosphere/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.uriatmosphere
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
@@ -12,9 +13,10 @@ import com.example.uriatmosphere.component.stationview.StationViewAdapter
 import com.example.uriatmosphere.databinding.ActivityMainBinding
 import com.example.uriatmosphere.openapi.ArpltnInforInqireSvc
 import com.example.uriatmosphere.openapi.model.MsrstnAcctoRltmMesureDnstyData
+import com.google.android.material.navigation.NavigationView
 import kotlin.concurrent.thread
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
     private var data: MsrstnAcctoRltmMesureDnstyData? = null
 
@@ -30,7 +32,9 @@ class MainActivity : AppCompatActivity() {
 
         // Toolbar 레이아웃 구성
         val appbar: Toolbar = binding.toolbar
+        val navigationView: NavigationView = binding.navigationView
         setSupportActionBar(appbar)
+        navigationView.setNavigationItemSelectedListener(this)
 
         // Station Card 레이아웃 구성
         viewManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
@@ -40,16 +44,48 @@ class MainActivity : AppCompatActivity() {
             layoutManager = viewManager
             adapter = viewAdapter
         }
+
     }
 
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             // 툴바의 네비게이션 버튼이 눌렸을 때
-            android.R.id.home->{
+            android.R.id.home -> {
                 binding.drawerLayout.openDrawer(GravityCompat.START)
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private var countClickBackButton: Int = 0
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawers()
+        } else {
+            if (countClickBackButton <= 0) {
+                Toast.makeText(this, "종료하려면 한번 더 눌러주세요.", Toast.LENGTH_SHORT).show()
+
+                countClickBackButton++
+            } else {
+                super.onBackPressed()
+            }
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.search_menu -> {
+                Toast.makeText(this, "search_menu clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.favorites_menu -> {
+                Toast.makeText(this, "favorites_menu clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.settings_menu -> {
+                Toast.makeText(this, "settings_menu clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return false
     }
 
     private fun updateData() {
